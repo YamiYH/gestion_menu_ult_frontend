08ef06f34405486c9ddca604c23d575dd801cea7
David Alejando <ucirivas@gmail.com>
2025-06-16 23:38:09 -0400

    style: remove unused import for UserProfile in Ventas

--- lib/screens/menu/Ventas/Ventas.dart
+++ lib/screens/menu/Ventas/Ventas.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:gestion_menu_ult_frontend/controllers/security/user/UserController.dart';
 import 'package:gestion_menu_ult_frontend/controllers/ticket/TicketController.dart';
 import 'package:gestion_menu_ult_frontend/models/UserEntity.dart';
-import 'package:gestion_menu_ult_frontend/models/UserProfile.dart';
 import 'package:gestion_menu_ult_frontend/widgets/CustomAppbar.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:uuid/uuid.dart';
